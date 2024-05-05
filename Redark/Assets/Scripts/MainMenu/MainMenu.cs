8c5e28b6ab291f1a77d21312fd45020fbb551959
Lucas Calumbi <lucas.calumbi@dcomp.ufs.br>
2024-05-05 15:40:23 -0300

    menu inicial implementado

--- Redark/Assets/Scripts/MainMenu/MainMenu.cs
+++ Redark/Assets/Scripts/MainMenu/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("SampleScene");
+        SceneManager.LoadSceneAsync("TileMap");
     }
 
     public void QuitGame()
